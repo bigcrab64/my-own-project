7239aefe56cc8cf86bfd1204bd750a1e387c6acf
Julian De La Torre <jujudlt3@gmail.com>
2022-09-12 09:17:32 -0700

    Initial Commit

---
+++ my own project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my own project
+//
+//  Created by De La Torre, Julian - Student on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
