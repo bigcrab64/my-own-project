0e0b0dc159cb529a81913f843017f2ac13c0b42f
Julian De La Torre <69283@psdschools.org>
2022-10-07 08:48:41 -0700

    mvp

--- my own project/AddTimeViewController.swift
+++ my own project/AddTimeViewController.swift
@@ -1,14 +1,15 @@
 //
-//  ViewController.swift
+//  AddTimeViewController.swift
 //  my own project
 //
-//  Created by De La Torre, Julian - Student on 9/12/22.
+//  Created by De La Torre, Julian - Student on 10/6/22.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddTimeViewController: UIViewController {
 
+    
     /*
      //MARK: FIRST INPUT
      */
@@ -19,8 +20,6 @@ class ViewController: UIViewController {
     @IBAction func minutesSlider(_ sender: UISlider) {
         minutes.resignFirstResponder()
         seconds.resignFirstResponder()
-        otherMinutes.resignFirstResponder()
-        otherSeconds.resignFirstResponder()
         sender.setValue(sender.value.rounded(.down), animated: true)
         let valueToInt = Int(minutesSliderText.value)
         minutes.text = String(valueToInt)
@@ -34,70 +33,36 @@ class ViewController: UIViewController {
     @IBAction func secondsSlider(_ sender: UISlider) {
         minutes.resignFirstResponder()
         seconds.resignFirstResponder()
-        otherMinutes.resignFirstResponder()
-        otherSeconds.resignFirstResponder()
         sender.setValue(sender.value.rounded(.down), animated: true)
         let valueToInt = Int(secondsSliderText.value)
         seconds.text = String(valueToInt)
     }
     
-    
-    /*
-     //MARK: SECOND INPUT
-     */
-    
-    
-    @IBOutlet weak var otherMinutes: UITextField!
-    
-    @IBOutlet weak var otherMinutesText: UISlider!
-    
-    @IBAction func otherMinutesSlider(_ sender: UISlider) {
-        minutes.resignFirstResponder()
-        seconds.resignFirstResponder()
-        otherMinutes.resignFirstResponder()
-        otherSeconds.resignFirstResponder()
-        sender.setValue(sender.value.rounded(.down), animated: true)
-        let valueToInt = Int(otherMinutesText.value)
-        otherMinutes.text = String(valueToInt)
-    }
-    
-    
-    @IBOutlet weak var otherSeconds: UITextField!
-    
-    @IBOutlet weak var otherSecondsText: UISlider!
-    
-    @IBAction func otherSecondsSlider(_ sender: UISlider) {
-        minutes.resignFirstResponder()
-        seconds.resignFirstResponder()
-        otherMinutes.resignFirstResponder()
-        otherSeconds.resignFirstResponder()
-        sender.setValue(sender.value.rounded(.down), animated: true)
-        let valueToInt = Int(otherSecondsText.value)
-        otherSeconds.text = String(valueToInt)
-    }
-    
-    
+ 
     @IBOutlet weak var Output: UILabel!
   
+    var toLog = ["entryNumber": [1, 2, 3]]
+    var entryCounter = 0
+    
+    
+    
     @IBAction func Add(_ sender: UIButton) {
+        
         minutes.resignFirstResponder()
         seconds.resignFirstResponder()
-        otherMinutes.resignFirstResponder()
-        otherSeconds.resignFirstResponder()
         
-        if minutes.text == "" || seconds.text == "" || otherMinutes.text == "" || otherSeconds.text == ""{
+        
+        if minutes.text == "" || seconds.text == ""{
             Output.text = "Invalid Input"
         }
         else{
-            if minutes.text!.count >= 4 || seconds.text!.count >= 4 || otherMinutes.text!.count >= 4 || otherSeconds.text!.count >= 4{
+            if minutes.text!.count >= 4 || seconds.text!.count >= 4{
                 Output.text = "Inputs can not exceed 3 digits"
             }
             else{
-                
+                entryCounter += 1
                 var thing1 = Int(minutes.text ?? "")
                 var thing2 = Int(seconds.text ?? "")
-                var thing3 = Int(otherMinutes.text ?? "")
-                var thing4 = Int(otherSeconds.text ?? "")
                 
                 var extraMinutes = 0
                 var remainingSecs = 0
@@ -106,7 +71,7 @@ class ViewController: UIViewController {
                 var extraHours = 0
                 var remainingMins = 0
                 
-                var addedSecs = thing2! + thing4!
+                var addedSecs = thing2!
                 if addedSecs >= 60{
                     extraMinutes = addedSecs / 60 //add this to minutes later
                     remainingSecs = addedSecs % 60 // this is final amount of seconds
@@ -116,7 +81,7 @@ class ViewController: UIViewController {
                 }
                 
                 
-                var addedMins = thing1! + thing3! + extraMinutes
+                var addedMins = thing1! + extraMinutes
                 if addedMins >= 60{
                     extraHours = addedMins / 60
                     remainingMins = addedMins % 60
@@ -128,7 +93,13 @@ class ViewController: UIViewController {
                 if addedMins >= 60{
                     if remainingMins < 10 && remainingSecs < 10{
                         Output.text = (String(extraHours) + ":0" + String(remainingMins) + ":0" + String(remainingSecs))
+                        
+                        
+                        
+                        
+                        
                         print(extraHours, remainingMins, remainingSecs)
+    
                     }
                     if remainingMins < 10 && remainingSecs >= 10{
                         Output.text = (String(extraHours) + ":0" + String(remainingMins) + ":" + String(remainingSecs))
@@ -152,18 +123,20 @@ class ViewController: UIViewController {
                         Output.text = (String(remainingMins) + ":0" + String(remainingSecs))
                         print(remainingMins, remainingSecs)
                     }
-                    
+                print(toLog)
                 }
+
             }
         }
     }
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
 
 }
-
